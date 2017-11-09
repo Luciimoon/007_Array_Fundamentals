2ca1069d9a9f3100f735791b454a1a27f2ceac4f
compsci <compsci@compsci>
2017-11-09 10:33:43 -0700

    Initial commit. forst part working

---
+++ src/TheMainClass.java
@@ -0,0 +1,23 @@
+
+
+
+public class TheMainClass {
+
+	public static void main(String[] args) {
+		
+		ArrayFun learnArrays = new ArrayFun();
+		
+		//learnArrays.showInstructions();
+		//learnArrays.intArrayExample();
+		
+		Matrix my2D = new Matrix();
+		//my2D.test2D();
+		//my2D.gameBoard();
+		my2D.realGame();
+	}
+
+	public static void confirmMain(String string) {
+		System.out.println(string);
+	}
+	
+}
